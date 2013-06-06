eb1a838ad63533f44889853d54864452eb81fef7
Andrew Yates <ayates@master.cl.osumc.edu>
2013-06-06 18:57:29 -0400

    time test results

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
+"""
+In [9]: %timeit COR = compute_all_ppc(M)
+1000 loops, best of 3: 811 us per loop
+
+In [10]: %timeit SCI = squareform((pdist(M, metric='correlation')-1)*-1)
+10000 loops, best of 3: 148 us per loop
+
+In [12]: %timeit DCOR =  compute_all_dcor(M)
+100 loops, best of 3: 5.01 ms per loop
+
+In [19]: %timeit DCORL = test.loop_dcor(M)
+10 loops, best of 3: 56.4 ms per loop
+"""
 from __init__ import *
 import matrix_io as mio
 import script
@@ -6,16 +19,26 @@ import numpy as np
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 FNAME="nice.may3.Eg.expr.gold.celegans.csv"
+import dcor
+M = mio.load(FNAME)['M']
+
+def loop_dcor(M):
+  m = M.shape[0]
+  D = np.zeros((m,m))
+  for i,rowi in enumerate(M):
+    for j,rowj in enumerate(M):
+      D[i,j] = dcor.dcor(rowi,rowj)
+  return D
 
 def main():
   DCOR = script.main(fname=FNAME)
   print DCOR
-  M = mio.load(FNAME)['M']
   COR = compute_all_ppc(M)
   print COR
   SCI = squareform((pdist(M, metric='correlation')-1)*-1)
   print SCI
   print COR-SCI
+  DCOR = compute_all_dcor(M)
 
 
   
