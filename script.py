a469467a65adeaf24c498ee393d33dc40e57d8c1
Andrew Yates <ayates@master.cl.osumc.edu>
2013-06-11 23:56:17 -0400

    algorithm switch script

--- script.py
+++ script.py
@@ -10,15 +10,29 @@ import matrix_io as mio
 from __init__ import *
 import cPickle as pickle
 
-def main(fname=None, pkl=True, **kwds):
+def main(fname=None, pkl=True, algorithm="1", outtag="", **kwds):
   assert fname
   if isinstance(pkl, basestring) and pkl.lower() in ('f','false','none'): pkl = False
   print "Loading data from %s..." % fname
   D = mio.load(fname)
   print "Computing all pairs (euclidean) distance correlation from a (%d x %d) data matrix to a (%d x %d) result matrix..." % (D['M'].shape[0], D['M'].shape[1], D['M'].shape[0], D['M'].shape[0])
   print "Computing all pairs (euclidean) distance correlation..."
-  DCOR = compute_all_dcor(D['M'], **kwds)
-  fname_out = '%s.dcor.tab' % (fname)
+  
+  if algorithm == "1":
+    print "Using Algorithm 1: single dot product, n^2*m memory"
+    DCOR = compute_all_dcor(D['M'], **kwds)
+  elif algorithm == "2":
+    print "Using Algorithm 2: multiple dot products, n*m memory"
+    DCOR = compute_all_dcor_2(D['M'], **kwds)
+  elif algorithm == "3":
+    print "Using Algorithm 3: multiple dot products, n*m memory, n choose 2 savings"
+    DCOR = compute_all_dcor_3(D['M'], **kwds)
+  else:
+    raise Exception, "Unknown algorithm %s" % algorithm
+
+  if outtag and outtag[-1] != ".":
+    outtag += "."
+  fname_out = '%s.%sdcor.tab' % (fname, outtag)
   print "Saving %s..." % (fname_out)
   mio.save(DCOR, fname_out, fmt="%.4f", row_ids=D['row_ids'], col_ids=D['row_ids'])
   if pkl:
