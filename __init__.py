b48ac84d5d3db84365431b28e3c066929102e5c7
Andrew Yates <andrewyates.name@gmail.com>
2013-06-11 23:31:56 -0400

    all_pairs memory optimization

--- __init__.py
+++ __init__.py
@@ -1,6 +1,54 @@
 #!/usr/bin/python
 import numpy as np
 
+def compute_all_dcor_2(M):
+  """Iteratively construct all-pairs distance matrices.
+  Medium overhead, but much lower memory usage than compute_all_dcor.
+  """
+  if not isinstance(M, np.matrix):
+    M = np.matrix(M)
+  m,n = M.shape
+  RowMeans = np.matrix(np.zeros((m,n)))
+  for j in xrange(n):
+    RowMeans[:,j] = np.mean(np.abs(M-M[:,j]),1)
+  DMeans = np.mean(RowMeans,1)
+  DCOV = np.zeros((m,m))
+  for j in xrange(n):
+    A = np.abs(M-M[:,j]) - RowMeans - RowMeans[:,j] + DMeans
+    DCOV += np.dot(A,A.T)
+  DCOV = np.sqrt(DCOV) / n
+  DSTD = np.sqrt(np.diag(DCOV))
+  DCOR = DCOV / DSTD / DSTD.reshape(m,1)
+  return DCOR
+
+def compute_all_dcor_3(M):
+  """Iteratively construct all-pairs distance matrices with n choose 2 savings.
+  Highest overhead, but algorithmically more efficient then compute_all_dcor2; same memory.
+  """
+  if not isinstance(M, np.matrix):
+    M = np.matrix(M)
+  m,n = M.shape
+  RowMeans = np.matrix(np.zeros((m,n)))
+  for j in xrange(n):
+    RowMeans[:,j] = np.mean(np.abs(M-M[:,j]),1)
+  DMeans = np.mean(RowMeans,1)
+  DCOV = np.zeros((m,m))
+
+  # upper triangle
+  for j in xrange(n-1):
+    A = np.abs(M[:,j+1:n]-M[:,j]) - RowMeans[:,j+1:n] - RowMeans[:,j] + DMeans
+    DCOV += np.dot(A,A.T)
+  DCOV *= 2
+  # diagonal
+  for j in xrange(n):
+    A = DMeans - 2*RowMeans[:,j]
+    DCOV += np.dot(A,A.T)
+  DCOV = np.sqrt(DCOV) # / n**2 (term cancels out in dCor division)
+  DSTD = np.sqrt(np.diag(DCOV))
+  DCOR = DCOV / DSTD / DSTD.reshape(m,1)
+  return DCOR
+                      
+    
 def compute_all_dcor(M):
   """Return matrix of all-row-pairs distance correlations."""
   Mat = np.matrix(M)
@@ -13,7 +61,7 @@ def compute_all_dcor(M):
     u = np.mean(D)
     D = D - u_row - u_col + u
     DIST[i,] = D.ravel()
-  DCOV = np.sqrt(np.dot(DIST,np.transpose(DIST)))
+  DCOV = np.sqrt(np.dot(DIST,np.transpose(DIST)))/n
   DSTD = np.sqrt(np.diag(DCOV))
   DCOR = DCOV / DSTD / DSTD.reshape(m,1)
   return DCOR
--- test.py
+++ test.py
@@ -18,8 +18,9 @@ import script
 import numpy as np
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
-FNAME="nice.may3.Eg.expr.gold.celegans.csv"
 import dcor
+
+FNAME="nice.may3.Eg.expr.gold.celegans.csv"
 M = mio.load(FNAME)['M']
 
 def loop_dcor(M):
@@ -32,16 +33,21 @@ def loop_dcor(M):
 
 def main():
   DCOR = script.main(fname=FNAME)
-  print DCOR
   COR = compute_all_ppc_numpy(M)
-  print COR
   SCI = compute_all_pcc_scipy(M)
   print np.all(np.abs(COR-SCI) < 0.0000000000001)
   DCOR = compute_all_dcor(M)
   DCORL = loop_dcor(M)
   print np.all(np.abs(DCOR-DCORL) < 0.0000000000001)
-
+  DCOR2 = compute_all_dcor_2(M)
+  print np.all(np.abs(DCOR-DCOR2) < 0.0000000000001)
+  DCOR3 = compute_all_dcor_3(M)
+  print np.all(np.abs(DCOR-DCOR3) < 0.0000000000001)
+  print np.all(np.abs(DCOR-DCOR3) < 0.1)
   
+  print DCOR[5,3], DCOR[3,5]
+  print DCOR3[5,3], DCOR3[3,5]
+
 if __name__ == "__main__":
   main()
 
